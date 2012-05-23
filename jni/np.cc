acdf3d64e7da049d4facf7a610ce345bd6fe6e7f
Tony Wallace <awallace@quicinc.com>
2012-05-23 12:11:14 -0700

    Prep. Release version 2.5.0

--- jni/np.cc
+++ jni/np.cc
@@ -90,7 +90,7 @@ NP_EXPORT(char*) NP_GetPluginVersion()
 {
     InitializeDebug();
     QCC_DbgPrintf(("%s", __FUNCTION__));
-    return const_cast<char*>("0.0.1");
+    return const_cast<char*>("2.5.0");
 }
 
 NP_EXPORT(char*) NP_GetMIMEDescription()
